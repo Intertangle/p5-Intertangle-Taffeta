836ed55042fddb7be1b9ed946b92dbc334e107bc
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-03-01 15:40:05 -0500

    Add link to repo info

--- lib/Renard/Taffeta.pm
+++ lib/Renard/Taffeta.pm
@@ -3,3 +3,8 @@ package Renard::Taffeta;
 # ABSTRACT: Low-level graphics layer
 
 1;
+__END__
+
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-Taffeta/>
