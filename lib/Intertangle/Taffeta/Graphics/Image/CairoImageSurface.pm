2c025f900f4f7e1f141c14cc2a585593f712eda7
Zakariyya Mughal <zaki.mughal@gmail.com>
2020-10-17 01:10:18 -0500

    Make author tests happy

--- lib/Intertangle/Taffeta/Graphics/Image/CairoImageSurface.pm
+++ lib/Intertangle/Taffeta/Graphics/Image/CairoImageSurface.pm
@@ -9,6 +9,11 @@ use Intertangle::Taffeta::Types qw(CairoContext);
 
 extends qw(Intertangle::Taffeta::Graphics::Image);
 
+=attr cairo_image_surface
+
+A L<Cairo> image surface.
+
+=cut
 has cairo_image_surface => (
 	is => 'ro',
 	isa => InstanceOf['Cairo::ImageSurface'],
